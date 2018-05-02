672ca33a2ba897621eae1baad2067bec87df8c9b
Hinrich Kaestner <hinrich.kaestner@gmail.com>
2018-05-02 22:12:27 +0200

    Fix "get started" message

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.jsx</code> and save to reload.
         </p>
         <Example name="Hinrich" />
       </div>
