4b315dcd6d719b9d75dfb3e5e426e53de261be68
Hinrich Kaestner <hinrich.kaestner@gmail.com>
2018-05-02 22:10:03 +0200

    setup of react project

---
+++ src/Components/Example/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+
+const Example = ({name}) =>
+    <div>
+        <h2 className="name">{name} was here</h2>
+    </div>
+
+export default Example
