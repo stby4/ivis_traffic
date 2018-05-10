b38c83334d5a1798efc551b5ffac8cc603f9149e
sandruen <sandraamport@gmail.com>
2018-05-10 22:46:13 +0200

    added labels, tried to add some html elements (didn't work) displaying circles on yAxis works. Tried to display dircles on every Year of xAxis, didn't work

--- src/Components/Visualization2/index.jsx
+++ src/Components/Visualization2/index.jsx
@@ -7,8 +7,8 @@ class Visualization2 extends Component {
     path: `${process.env.PUBLIC_URL}/StrassenverkehrsunfaelleMaengel.csv`,
     canvHeight: 550,
     canvWidth: 800,
-    margin: { top: 50, right: 20, bottom: 30, left: 60 },
-    height: 550 - 50 - 30,
+    margin: { top: 50, right: 20, bottom: 50, left: 60 },
+    height: 550 - 50 - 50,
     width: 800 - 60 - 20,
   }
 
@@ -19,7 +19,7 @@ class Visualization2 extends Component {
       objektart1: "Personenwagen",
       strassenart1: "Autobahn",
       unfallschwere1: "Unfall mit leicht Verletzten",
-      jahr1: 2018,
+      jahr1: 1994,
       data: null,
     }
   }
@@ -27,7 +27,7 @@ class Visualization2 extends Component {
   componentDidMount() {
     d3.csv(this.props.path)
       .then(data => {
-        this.setState({ data: data }) // now this.state.data can be used in the render() function
+        this.setState({ data: data }) // now this.state.data can be used in the render() function;
       })
   }
 
@@ -50,9 +50,9 @@ class Visualization2 extends Component {
       filteredData = filteredData.filter(row => row['Strassenart'] === strassenart1)
       filteredData = filteredData.filter(row => row['Unfallschwere'] === unfallschwere1)
       console.log("Show me some data, Unfallschwere: " + filteredData)
-      filteredData = filteredData.filter(row => row['Unfalltyp'] === 'Schleuder-, Selbstunfall')
-      console.log("Show me some data, nur noch 1 Zeile mit Unfalltyp: " + filteredData)
-      console.log("Show me some data, 1 Feld: " + filteredData[0]['1992'])
+      let moreFilteredData = filteredData.filter(row => row['Unfalltyp'] === 'Schleuder-, Selbstunfall')
+      //console.log("Show me some data, nur noch 1 Zeile mit Unfalltyp: " + filteredData)
+      console.log("Show me some data, 1 Feld: " + moreFilteredData[0]['1992'])
 
       var colsYears = data.map(function(d) {
         return {
@@ -63,7 +63,6 @@ class Visualization2 extends Component {
         }
       });
 
-
       const einfluss = d3.extent(data, d => d["Mangel oder Einfluss"])
       const objektart = d3.extent(data, d => d["Objektart"])
       const strassenart = d3.extent(data, d => d["Strassenart"])
@@ -72,38 +71,66 @@ class Visualization2 extends Component {
 
       // create scale for x and y direction
       const xScale = d3.scaleTime()
-        .domain([new Date("1992"), new Date("2016")])
+        .domain([new Date("1991"), new Date("2016")])
         .rangeRound([0, width])
 
       const yScale = d3.scaleLinear()
-        .domain([0, 1200])
+        .domain([0, 800])
         .rangeRound([height, 0])
 
       const g = d3.select("#chart-area")
-
-      const zScale = d3.scaleLinear()
-        .domain([0, 1200])
-        .rangeRound([900, 0])
-      const zAxis = d3.axisLeft(zScale)
 
       // create xAxis and yAxis
       const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"))
       g.select("#axisX").call(xAxis)
 
+
       const yAxis = d3.axisLeft(yScale)
       g.select("#axisY").call(yAxis) // evtl. umschreiben, so dass select nicht mehr verwendet wird
+
+      g.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x",0 - (height / 2))
+      .attr("y", 0 - margin.left)
+      .attr("class", "label")
+      .attr("dy", "1em")
+      .attr("font-family", "sans-serif")
+      .style("text-anchor", "middle")
+      .text("Anzahl Unfälle ");
+
+      g.append("text")
+      .attr("x", width/2)
+      .attr("y", height + 25)
+      .attr("class", "label")
+      .attr("dy", "1em")
+      .attr("font-family", "sans-serif")
+      .style("text-anchor", "middle")
+      .text("Jahre");
+
+      console.log("wert?: "+ colsYears[1][1992]);
 
       g.selectAll("circle") // evtl. umschreiben, so das selectAll nicht mehr verwendet wird
         .data(filteredData)
         .enter().append("circle")
-        .attr("cx", 50)
-        .attr("cy", d => yScale(d['1992']))
+        // Für jedes Datum auf der x-Achse soll ein circle gezeichnet werden.
+        .attr("cx", function(d) { return xScale(d.date); })
+        .attr("cx", 60)
+        .attr("cy", d => yScale(d['1996']))
         .attr("r", 4)
         .style("fill", "#b0cccc")
     }
+    
 
     return (
       <svg id={svgId} width={canvWidth} height={canvHeight} style={{ align: 'center' }}>
+        <div id="selection-area">
+          <div>
+            <select>
+              <option value="bla1">bla1</option>
+              <option value="bla2">bla2</option>
+            </select>
+          </div>
+        </div>
         <g id="chart-area" transform={`translate(${margin.left},${margin.top})`}>
           <g id="axisX" className="axis" transform={`translate(0,${height})`} />
           <g id="axisY" className="axis" />
