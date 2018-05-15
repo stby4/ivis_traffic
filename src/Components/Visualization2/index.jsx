7369e44f4cd0d124e0f1e38cd88c1740b831adcb
sandruen <sandraamport@gmail.com>
2018-05-15 12:24:44 +0200

    filled selection-are with data displayed a line

--- src/Components/Visualization2/index.jsx
+++ src/Components/Visualization2/index.jsx
@@ -55,41 +55,41 @@ class Visualization2 extends Component {
       //console.log("Show me some data, nur noch 1 Zeile mit Unfalltyp: " + filteredData)
       console.log("Show me some data, 1 Feld: " + moreFilteredData[0]['1992'])
 
-      var colsYears = filteredData.map(function (d) {
-        return {
-          1992: d['1992'],
-          1993: d['1993'],
-          1994: d['1994'],
-          1995: d['1995'],
-          1996: d['1996'],
-        }
-      })
-
+      // Das brauchen wir nicht, oder?
       const einfluss = d3.extent(data, d => d["Mangel oder Einfluss"])
       const objektart = d3.extent(data, d => d["Objektart"])
       const strassenart = d3.extent(data, d => d["Strassenart"])
       const unfallschwere = d3.extent(data, d => d["Unfallschwere"])
       const jahr1992 = d3.extent(data, d => d["1992"])
 
-      // create scale for x and y direction
+      // create scale for x direction
       const xScale = d3.scaleTime()
         .domain([new Date("1991"), new Date("2016")])
         .rangeRound([0, width])
 
+      // create scale for y direction
       const yScale = d3.scaleLinear()
         .domain([0, 800])
         .rangeRound([height, 0])
 
+      // select chart-area
       const g = d3.select("#chart-area")
 
-      // create xAxis and yAxis
+      // define the line
+      var valueline = d3.line()
+        .x((d, i) => { return xScale(new Date('' + (1992 + i))) })
+        .y((d, i) => { return yScale(d) })
+        .curve(d3.curveCatmullRom)
+
+      // create xAxis
       const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"))
       g.select("#axisX").call(xAxis)
 
-
+      // create yAxis
       const yAxis = d3.axisLeft(yScale)
       g.select("#axisY").call(yAxis) // evtl. umschreiben, so dass select nicht mehr verwendet wird
 
+      // Add label for yAxis
       g.append("text")
         .attr("transform", "rotate(-90)")
         .attr("x", 0 - (height / 2))
@@ -100,6 +100,7 @@ class Visualization2 extends Component {
         .style("text-anchor", "middle")
         .text("Anzahl Unfälle ")
 
+      // Add label for xAxis
       g.append("text")
         .attr("x", width / 2)
         .attr("y", height + 25)
@@ -109,10 +110,9 @@ class Visualization2 extends Component {
         .style("text-anchor", "middle")
         .text("Jahre")
 
-      console.log("wert?: " + colsYears[1][1992])
-
       const theData = Object.values(filteredData[1]).slice(0, 2016 - 1992)
-      // debugger
+      
+      // Add the circles
       g.selectAll("circle")
         .data(theData)
         .enter()
@@ -120,7 +120,16 @@ class Visualization2 extends Component {
         .attr("r", 4)
         .attr("cx", (d, i) => { /*debugger;*/ return xScale(new Date('' + (1992 + i))) })
         .attr("cy", (d, i) => { /*debugger;*/ return yScale(d) })
-        .style("fill", "#b0cccc")
+        .style("fill", "#F0F0F0")
+
+      // Add the valueline path.
+      g.append("path")
+        .data([theData])
+        .attr("class", "lines")
+        .attr("stroke", "#F0F0F0")
+        .attr("stroke-width", "1.5px")
+        .attr("fill", "none")
+        .attr("d", valueline)
     }
 
 
@@ -131,31 +140,51 @@ class Visualization2 extends Component {
           <div class="selection-group">
             <h3 class="selection-group-title">Objektart</h3>
             <div class="selection-items" id="Objektart" role="tablist">
-              <a class="item">Personenwagen</a>
-              <a class="item">Kleinmotorrad</a>
-              <a class="item">Motorrad</a>
-              <a class="item">Fahrrad</a>
-              <a class="item">Anderes</a>
+              <a class="item" id="Personenwagen">Personenwagen</a>
+              <a class="item" id="Personentransportfahrzeuge">Personentransportfahrzeuge</a>
+              <a class="item" id="Sachentransportfahrzeuge">Sachentransportfahrzeuge</a>
+              <a class="item" id="Kleinmotorrad">Kleinmotorrad</a>
+              <a class="item" id="Motorrad bis 125 ccm">Motorrad bis 125 ccm</a>
+              <a class="item" id="Motorrad über 125 ccm">Motorrad über 125 ccm</a>
+              <a class="item" id="Fahrrad">Fahrrad</a>
+              <a class="item" id="Motorfahrrad">Motorfahrrad</a>
+              <a class="item" id="FussgängerIn">FussgängerIn</a>
+              <a class="item" id="Anderes nicht motorisiertes Fahrzeug">Anderes nicht motorisiertes Fahrzeug</a>
+              <a class="item" id="Andere und unbekannte Fahrzeuge">Andere und unbekannte Fahrzeuge</a>
+          
             </div>
           </div>
           <div class="selection-group">
             <h3 class="selection-group-title">Unfallschwere</h3>
-            <div class="selection-item" id="Unfallschwere" role="tablist">
-              <a class="item">leicht Verletzte</a>
-              <a class="item">schwer Verletzte</a>
-              <a class="item">Getötete</a>  
+            <div class="selection-items" id="Unfallschwere" role="tablist">
+              <a class="item" id="Unfall mit leicht Verletzten">Unfall mit leicht Verletzten</a>
+              <a class="item" id="Unfall mit schwer Verletzten">Unfall mit schwer Verletzten</a>
+              <a class="item" id="Unfall mit Getoeteten">Unfall mit Getöteten</a>  
             </div>
           </div>
           <div class="selection-group">
             <h3 class="selection-group-title">Strassenart</h3>
-            <div class="selection-item" id="Strassenart" role="tablist">
-              <a class="item">Autobahn</a>
+            <div class="selection-items" id="Strassenart" role="tablist">
+              <a class="item" id="Autobahn">Autobahn</a>
+              <a class="item" id="Autostrasse">Autostrasse</a>
+              <a class="item" id="Hauptstrasse">Hauptstrasse</a>
+              <a class="item" id="Nebenstrasse">Nebenstrasse</a>
+              <a class="item" id="Andere Strasse">Andere Strasse</a>
             </div>
           </div>
           <div class="selection-group">
             <h3 class="selection-group-title">Unfalltyp</h3>
-            <div class="selection-item" id="Unfalltyp" role="tablist">
-              <a class="item">Ueberholunfall</a>
+            <div class="selection-items" id="Unfalltyp" role="tablist">
+              <a class="item" id="Fussgaengerunfall">Fussgängerunfall</a>
+              <a class="item" id="Schleuder-, Selbstunfall">Schleuder-, Selbstunfall</a>
+              <a class="item" id="Beim Kreuzen (in Laengsrichtung)">Beim Kreuzen (in Längsrichtung)</a>
+              <a class="item" id="Ueberholunfall">Überholunfall</a>
+              <a class="item" id="Auffahrunfall">Auffahrunfall</a>
+              <a class="item" id="Beim Vorbeifahren, Fahrstreifenwechsel">Beim Vorbeifahren, Fahrstreifenwechsel</a>
+              <a class="item" id="Beim Richtungswechsel (mit Abbiegen)">Beim Richtungswechsel (mit Abbiegen)</a>
+              <a class="item" id="Beim Queren (ohne Abbiegen)">Beim Queren (ohne Abbiegen)</a>
+              <a class="item" id="Tierunfall">Tierunfall</a>
+              <a class="item" id="Andere">Andere</a>
             </div>
           </div>
         </div>
